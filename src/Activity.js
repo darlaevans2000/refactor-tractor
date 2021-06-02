c5bd9334b23181d7468ab8a18773858bead552fd
Ashton Huxtable <aehuxtable@gmail.com>
2021-06-02 15:02:27 -0600

    Update class files to hold user methods

--- src/Activity.js
+++ src/Activity.js
@@ -7,6 +7,13 @@ class Activity {
     this.flightsOfStairs = data.flightsOfStairs;
     this.milesWalked = 0;
     this.reachedStepGoal = null;
+    //  this.totalStepsThisWeek = 0; 
+    // this.activityRecord = []; - activity
+    // this.accomplishedDays = []; - activity (stepsExceededDays???)
+    // this.trendingStepDays = []; -  # of days in a row that step goal has been exceeded - activity
+    // this.trendingStairsDays = []; - # of days in a row that ?????  - activity
+    // this.friendsActivityRecords = [] - activity
+
     this.doActivity(userRepository);
   }
   doActivity(userRepo) {
@@ -27,6 +34,103 @@ class Activity {
     }).dailyStepGoal;
     this.reachedStepGoal = this.steps >= userStepGoal;
   }
+
+  // updateActivities(activity) {
+  //   this.activityRecord.unshift(activity);
+  //   if (activity.numSteps >= this.dailyStepGoal) {
+  //     this.accomplishedDays.unshift(activity.date);
+  //   }
+  // }
+  // findClimbingRecord() {
+  //   return this.activityRecord.sort((a, b) => {
+  //     return b.flightsOfStairs - a.flightsOfStairs;
+  //   })[0].flightsOfStairs;
+  // }
+  // calculateDailyCalories(date) {
+  //   let totalMinutes = this.activityRecord.filter(activity => {
+  //     return activity.date === date
+  //   }).reduce((sumMinutes, activity) => {
+  //     return sumMinutes += activity.minutesActive
+  //   }, 0);
+  //   return Math.round(totalMinutes * 7.6);
+  // }
+  // calculateAverageMinutesActiveThisWeek(todayDate) {
+  //   return (this.activityRecord.reduce((sum, activity) => {
+  //     let index = this.activityRecord.indexOf(this.activityRecord.find(activity => activity.date === todayDate));
+  //     if (index <= this.activityRecord.indexOf(activity) && this.activityRecord.indexOf(activity) <= (index + 6)) {
+  //       sum += activity.minutesActive;
+  //     }
+  //     return sum;
+  //   }, 0) / 7).toFixed(0);
+  // }
+  // calculateAverageStepsThisWeek(todayDate) {
+  //   return (this.activityRecord.reduce((sum, activity) => {
+  //     let index = this.activityRecord.indexOf(this.activityRecord.find(activity => activity.date === todayDate));
+  //     if (index <= this.activityRecord.indexOf(activity) && this.activityRecord.indexOf(activity) <= (index + 6)) {
+  //       sum += activity.steps;
+  //     }
+  //     return sum;
+  //   }, 0) / 7).toFixed(0);
+  // }
+  // calculateAverageFlightsThisWeek(todayDate) {
+  //   return (this.activityRecord.reduce((sum, activity) => {
+  //     let index = this.activityRecord.indexOf(this.activityRecord.find(activity => activity.date === todayDate));
+  //     if (index <= this.activityRecord.indexOf(activity) && this.activityRecord.indexOf(activity) <= (index + 6)) {
+  //       sum += activity.flightsOfStairs;
+  //     }
+  //     return sum;
+  //   }, 0) / 7).toFixed(1);
+  // }
+  // findTrendingStepDays() {
+  //   let positiveDays = [];
+  //   for (var i = 0; i < this.activityRecord.length; i++) {
+  //     if (this.activityRecord[i + 1] && this.activityRecord[i].steps > this.activityRecord[i + 1].steps) {
+  //       positiveDays.unshift(this.activityRecord[i].date);
+  //     } else if (positiveDays.length > 2) {
+  //       this.trendingStepDays.push(`Your most recent positive step streak was ${positiveDays[0]} - ${positiveDays[positiveDays.length - 1]}!`);
+  //       positiveDays = [];
+  //     }
+  //   }
+  // }
+  // findTrendingStairsDays() {
+  //   let positiveDays = [];
+  //   for (var i = 0; i < this.activityRecord.length; i++) {
+  //     if (this.activityRecord[i + 1] && this.activityRecord[i].flightsOfStairs > this.activityRecord[i + 1].flightsOfStairs) {
+  //       positiveDays.unshift(this.activityRecord[i].date);
+  //     } else if (positiveDays.length > 2) {
+  //       this.trendingStairsDays.push(`Your most recent positive climbing streak was ${positiveDays[0]} - ${positiveDays[positiveDays.length - 1]}!`);
+  //       positiveDays = [];
+  //     }
+  //   }
+  // }
+  // calculateTotalStepsThisWeek(todayDate) {
+  //   this.totalStepsThisWeek = (this.activityRecord.reduce((sum, activity) => {
+  //     let index = this.activityRecord.indexOf(this.activityRecord.find(activity => activity.date === todayDate));
+  //     if (index <= this.activityRecord.indexOf(activity) && this.activityRecord.indexOf(activity) <= (index + 6)) {
+  //       sum += activity.steps;
+  //     }
+  //     return sum;
+  //   }, 0));
+  // }
+  // findFriendsTotalStepsForWeek(users, date) {
+  //   this.friends.map(friend => {
+  //     let matchedFriend = users.find(user => user.id === friend);
+  //     matchedFriend.calculateTotalStepsThisWeek(date);
+  //     this.friendsActivityRecords.push(
+  //       {
+  //         'id': matchedFriend.id,
+  //         'firstName': matchedFriend.name.toUpperCase().split(' ')[0],
+  //         'totalWeeklySteps': matchedFriend.totalStepsThisWeek
+  //       })
+  //   })
+  //   this.calculateTotalStepsThisWeek(date);
+  //   this.friendsActivityRecords.push({
+  //     'id': this.id,
+  //     'firstName': 'YOU',
+  //     'totalWeeklySteps': this.totalStepsThisWeek
+  //   });
+  //   this.friendsActivityRecords = this.friendsActivityRecords.sort((a, b) => b.totalWeeklySteps - a.totalWeeklySteps);
+  // }
 }
 
 export default Activity;
