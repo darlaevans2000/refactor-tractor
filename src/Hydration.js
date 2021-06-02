ab398dfa19deb8b30edc77c6ec1610a27d240a9d
Lauren Kessell <lkessell1@gmail.com>
2021-06-02 17:51:07 -0400

    Refactor average ounces per day for all time

--- src/Hydration.js
+++ src/Hydration.js
@@ -3,17 +3,37 @@ class Hydration {
     this.userId = data.userID;
     this.date = data.date;
     this.ounces = data.numOunces;
+    this.data = data;
     // this.ouncesAverage = 0;  - hydration
     // this.ouncesRecord = []; - hydration
 
     this.drink(userRepository);
   }
-  drink(userRepo) {
-    var hydrate = this;
-    userRepo.users.find(function(user) {
-      return user.id === hydrate.userId;
-    }).updateHydration(this.date, this.ounces);
+  // Calculate average ounces per day for all time
+  calcDailyAvgOz(id) {
+    let totalDays = 0;
+
+    let totalOz = this.data.reduce((sum, dayData) => {
+      if (dayData.userID === id) {
+        totalDays += 1;
+        sum += dayData.numOunces;
+      }
+      return sum;
+    }, 0);
+
+    return Math.floor(totalOz / totalDays);
   }
+
+  // Find how many ounces consumed on a specific day
+
+  // Find how many ounces consumed on each day of a given week
+
+  // drink(userRepo) {
+  //   var hydrate = this;
+  //   userRepo.users.find(function(user) {
+  //     return user.id === hydrate.userId;
+  //   }).updateHydration(this.date, this.ounces);
+  // }
 
   // updateHydration(date, amount) {
   //   this.ouncesRecord.unshift({[date]: amount});
@@ -23,6 +43,8 @@ class Hydration {
   //     this.ouncesAverage = amount;
   //   }
   // }
+
+  // Average Oz?
   // addDailyOunces(date) {
   //   return this.ouncesRecord.reduce((sum, record) => {
   //     let amount = record[date];
