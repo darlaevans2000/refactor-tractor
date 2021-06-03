047658ee63bf4adf32aa2300d1af925d113f9e7f
Lauren Kessell <lkessell1@gmail.com>
2021-06-03 19:24:17 -0400

    Update class with Data Repository inheritance

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,59 +1,17 @@
-class Hydration {
-  constructor(data, userRepository) {
-    this.userId = data.userID;
-    this.date = data.date;
-    this.ounces = data.numOunces;
-    this.data = data;
-    // this.ouncesAverage = 0;  - hydration
-    // this.ouncesRecord = []; - hydration
-
-    this.drink(userRepository);
+class Hydration extends DataRepository {
+  constructor(id, dataSet) {
+    super(id, dataSet);
   }
+
   // Calculate average ounces per day for all time
-  calcDailyAvgOz(id) {
-    let totalDays = 0;
-
-    let totalOz = this.data.reduce((sum, dayData) => {
-      if (dayData.userID === id) {
-        totalDays += 1;
-        sum += dayData.numOunces;
-      }
-      return sum;
-    }, 0);
-
-    return Math.floor(totalOz / totalDays);
+  getAllTimeAvgOunces(property) {
+    super.getAllTimeAvgDaily(property);
   }
 
   // Find how many ounces consumed on a specific day
 
   // Find how many ounces consumed on each day of a given week
 
-  // drink(userRepo) {
-  //   var hydrate = this;
-  //   userRepo.users.find(function(user) {
-  //     return user.id === hydrate.userId;
-  //   }).updateHydration(this.date, this.ounces);
-  // }
-
-  // updateHydration(date, amount) {
-  //   this.ouncesRecord.unshift({[date]: amount});
-  //   if (this.ouncesRecord.length) {
-  //     this.ouncesAverage = Math.round((amount + (this.ouncesAverage * (this.ouncesRecord.length - 1))) / this.ouncesRecord.length);
-  //   } else {
-  //     this.ouncesAverage = amount;
-  //   }
-  // }
-
-  // Average Oz?
-  // addDailyOunces(date) {
-  //   return this.ouncesRecord.reduce((sum, record) => {
-  //     let amount = record[date];
-  //     if (amount) {
-  //       sum += amount
-  //     }
-  //     return sum
-  //   }, 0)
-  // }
 }
 
 export default Hydration;
