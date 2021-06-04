62909b54e3b722ceaab37b24173d8594f037c158
Lauren Kessell <lkessell1@gmail.com>
2021-06-04 15:42:13 -0400

    Refactor imports and beforeEach

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -1,63 +1,19 @@
 import { expect } from 'chai';
 
+import DataRepository from '../src/DataRepository';
 import Sleep from '../src/Sleep';
-import UserRepository from '../src/UserRepository';
 import User from '../src/User';
+import testSleepData from './test-data/test-sleep';
+import testUserData from './test-data/test-users';
 
-describe('Sleep', function() {
-  let sleep;
-  let user1;
-  let user2;
-  let userRepository;
+describe.only('Sleep', function() {
+  let sleepData, user1;
+
   beforeEach(() => {
-    user1 = new User({
-      'id': 1,
-      'name': 'Luisa Hane',
-      'address': '15195 Nakia Tunnel, Erdmanport VA 19901-1697',
-      'email': 'Diana.Hayes1@hotmail.com',
-      'strideLength': 4.3,
-      'dailyStepGoal': 10000,
-      'friends': [
-        16,
-        4,
-        8
-      ]
-    });
-    user2 = new User({
-      "id": 2,
-      "name": "Jarvis Considine",
-      "address": "30086 Kathryn Port, Ciceroland NE 07273",
-      "email": "Dimitri.Bechtelar11@gmail.com",
-      "strideLength": 4.5,
-      "dailyStepGoal": 5000,
-      "friends": [
-        9,
-        18,
-        24,
-        19
-      ]
-    })
-    userRepository = new UserRepository();
-    userRepository.users.push(user1, user2);
-    sleep1 = new Sleep({
-      "userID": 1,
-      "date": "2019/06/15",
-      "hoursSlept": 6.1,
-      "sleepQuality": 2.2
-    }, userRepository);
-    sleep2 = new Sleep({
-      "userID": 2,
-      "date": "2019/06/25",
-      "hoursSlept": 7.3,
-      "sleepQuality": 3.2
-    }, userRepository);
-    sleep3 = new Sleep({
-      "userID": 1,
-      "date": "2019/07/17",
-      "hoursSlept": 9.3,
-      "sleepQuality": 1.4
-    }, userRepository);
+    sleepData = new Sleep(1, testSleepData);
+    user1 = new User(testUserData[1]);
   });
+
   it('should be a function', function() {
     expect(Sleep).to.be.a('function');
   });
