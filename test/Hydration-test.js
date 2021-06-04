ea3445bcbefb55fc0d0443ed6522762283fcc2da
Ashton Huxtable <aehuxtable@gmail.com>
2021-06-04 15:40:06 -0600

    Pass tests for hydration class

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -23,23 +23,34 @@ describe.only('Hydration', () => {
   it('should be an instance of Hydration', () => {
     expect(hydration).to.be.an.instanceof(Hydration);
   });
-  it('should have an id', () => {
-    expect(hydration.id).to.equal(1);
+  it('should have the same userID for the dataset', () => {
+    let day1Data = hydration.userData[0]
+    let day10Data = hydration.userData[9]
+    expect(day1Data.userID).to.equal(1);
+    expect(day10Data.userID).to.equal(1);
   });
 
   it('should be able to find the overall average H2O consumed by a user', () => {
-    hydration.getAllTimeAvgOz(testHydrationData.numOunces);
+    hydration.getAllTimeAvgOz();
     expect(hydration.getAllTimeAvgOz()).to.equal(60);
   });
 
   it('should find the number of ounces drank on a given day', () => {
-    hydration.getOzOnDate("2019/06/17", testHydrationData.numOunces);
-    expect(hydration.getOzOnDate()).to.equal(96);
+    let waterOnDate = hydration.getOzOnDate("2019/06/17", 'numOunces');
+    expect(waterOnDate).to.equal(96);
   });
 
   it('should find the number of ounces drank per day for any given week', () => {
-    hydration.getWeekOfOz("2019/06/24",testHydrationData.numOunces);
-    expect(weekValues.length).to.equal(7);
+    let weekOfWater = hydration.getWeekOfOz("2019/06/24",'numOunces');
+    expect(weekOfWater.length).to.equal(7);
+  })
+
+  it('should return an error if there is no data for the date input' () => {
+
+  })
+
+  it('should return an error if the date input does not match the format' () => {
+
   })
 
   // it('should have a date', () => {
