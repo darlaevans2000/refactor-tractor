53914222b2b5976f2722b4ee0fa53e72fdf5981a
Lauren Kessell <lkessell1@gmail.com>
2021-06-04 20:27:41 -0400

    Refactor some methods

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -31,42 +31,56 @@ class UserRepository {
     return parseFloat((sumQuality / sleepData.length).toFixed(1));
   }
 
-  calculateAverageActivity(date, task) {
-    let count = this.users.map(user => {
-      return user.activityRecord.filter(activity => activity.date === date)
-    });
-
-    let sum = count.reduce((total, activityList) => {
-      activityList.forEach(activity => {
-        total += activity[task];
-      });
-      return total;
-    }, 0);
-
-    return Math.round(sum / count.length);
-  }
+  // calculateAverageActivity(date, task) {
+  //   let count = this.users.map(user => {
+  //     return user.activityRecord.filter(activity => activity.date === date)
+  //   });
+  //
+  //   let sum = count.reduce((total, activityList) => {
+  //     activityList.forEach(activity => {
+  //       total += activity[task];
+  //     });
+  //     return total;
+  //   }, 0);
+  //
+  //   return Math.round(sum / count.length);
+  // }
   // use for average steps, stairs, and minutes active
+  calculateAverageActivity(dataSet, date, task) {
+    let matchedDates = dataSet.filter(element => element.date === date);
 
+    let taskValues = matchedDates.map(element => element[task]);
+
+    let sum = taskValues.reduce((total, currentValue) => total += currentValue, 0);
+
+    return Math.round(sum / taskValues.length);
+  }
 
 // *** Activity record is now a part of activity class not user class
 
-
-  calculateAverageDailyWater(date) {
-    let todaysDrinkers = this.users.filter(user => user.addDailyOunces(date) > 0);
-    let sumDrankOnDate = todaysDrinkers.reduce((sum, drinker) => {
-      return sum += drinker.addDailyOunces(date);
-    }, 0)
-    return Math.floor(sumDrankOnDate / todaysDrinkers.length);
-  }
+// This isn't in the spec anywhere?
+  // calculateAverageDailyWater(date) {
+  //   let todaysDrinkers = this.users.filter(user => user.addDailyOunces(date) > 0);
+  //   let sumDrankOnDate = todaysDrinkers.reduce((sum, drinker) => {
+  //     return sum += drinker.addDailyOunces(date);
+  //   }, 0)
+  //   return Math.floor(sumDrankOnDate / todaysDrinkers.length);
+  // }
 
   findBestSleepers(date) {
     return this.users.filter(user => user.calculateAverageQualityThisWeek(date) > 3);
   }
 
-  getLongestSleepers(date) {
-    sleepData.filter(sleep => sleep.date === date).sort((a, b) => {
-      return b.hoursSlept - a.hoursSlept;
-    })[0].userID;
+  // getLongestSleepers(date, sleepData) {
+  //   sleepData.filter(sleep => sleep.date === date).sort((a, b) => {
+  //     return b.hoursSlept - a.hoursSlept;
+  //   })[0].userID;
+  // }
+  getLongestSleepers(date, sleepData) {
+    let dateMatch = sleepData.filter(sleep => sleep.date === date);
+    let sortedMatches = dateMatch.sort((a, b) => b.hoursSlept - a.hoursSlept);
+
+    return sortedMatches[0].userID;
   }
 
   getWorstSleepers(date) {
