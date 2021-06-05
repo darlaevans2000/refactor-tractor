39d77aee17544af2fffd1dd4537a0e4a2e478578
Lauren Kessell <lkessell1@gmail.com>
2021-06-05 17:32:23 -0400

    Add tests for properties and instantiation

--- test/DataRepository-test.js
+++ test/DataRepository-test.js
@@ -1,7 +1,37 @@
 import { expect } from 'chai';
 
-import DataRepository from '../src/DataRepository'
+import DataRepository from '../src/DataRepository';
+import testSleepData from './test-data/test-sleep';
 
-describe('DataRepository', () => {
+describe.only('DataRepository', () => {
+  let dataRepo;
+
+  beforeEach(() => {
+    dataRepo = new DataRepository(2, testSleepData);
+  });
+
+  it('Should be a function', () => {
+    expect(DataRepository).to.be.a('function');
+  });
+
+  it('Should be an instance of DataRepository', () => {
+    expect(dataRepo).to.be.an.instanceof(DataRepository);
+  });
+
+  it('Should contain user data', () => {
+    expect(dataRepo.userData[0]).to.deep.equal({
+      'userID': 2,
+      'date': '2019/06/15',
+      'hoursSlept': 7,
+      'sleepQuality': 4.7
+    });
+  });
+
+  it('Should contain data for a particular user ID', () => {
+    expect(dataRepo.userData[1].userID).to.equal(2);
+    expect(dataRepo.userData[2].userID).to.equal(2);
+    expect(dataRepo.userData[3].userID).to.equal(2);
+  });
+
 
 });
